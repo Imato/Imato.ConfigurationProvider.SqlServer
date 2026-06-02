5fb52f11a16b8b2ce53cf2d3115957e3534cf198
Alexey Varentsov <alexey.varentsov@gmail.com>
2026-06-02 08:44:36 +0300

    var 1.0.2

--- src/Imato.ConfigurationProvider.SqlServer/AppBuilderExtensions.cs
+++ src/Imato.ConfigurationProvider.SqlServer/AppBuilderExtensions.cs
@@ -4,6 +4,12 @@ namespace Imato.ConfigurationProvider.SqlServer;
 
 public static class AppBuilderExtensions
 {
+    /// <summary>
+    /// Add configurations from DB table
+    /// </summary>
+    /// <param name="manager"></param>
+    /// <param name="configurationFactory"></param>
+    /// <returns></returns>
     public static IConfigurationManager AddSqlConfigurations(
         this IConfigurationManager manager,
         Action<SqlConfigurationOptions>? configurationFactory = null)
--- src/Imato.ConfigurationProvider.SqlServer/ConfigExtensions.cs
+++ src/Imato.ConfigurationProvider.SqlServer/ConfigExtensions.cs
@@ -4,6 +4,14 @@ namespace Imato.ConfigurationProvider.SqlServer;
 
 public static class ConfigExtensions
 {
+    /// <summary>
+    /// Find class T by name in Configurations
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="configuration"></param>
+    /// <param name="sectionName"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
     public static T? GetValueOf<T>(this IConfiguration configuration, string? sectionName = null)
     {
         var name = sectionName ?? typeof(T).Name;
